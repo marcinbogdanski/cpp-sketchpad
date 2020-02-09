106f26eb3b6235fd88f3c5d86418fc00a181dc74
Marcin Bogdanski <marcin.bogdanski@gmail.com>
2020-02-09 20:58:05 +0000

    Add vector dependancy

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
 int main(){
     cout << "Hello World" << endl;
+
+    vector<string> msg{"Hello", "Vector", "String"};
+
+    for (const string& word : msg){
+        cout << word << " ";
+    }
+    cout << endl;
+    
     return 0;
 }
